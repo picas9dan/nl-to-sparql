1eee7be6e2d6dc7031dcc29930b40947570bace7
Dan Tran <nmdt2@cam.ac.uk>
2023-12-27 16:58:12 +0800

    Expand for when querying for property

--- core/data_processing/ontobuiltenv/compact2verbose.py
+++ core/data_processing/ontobuiltenv/compact2verbose.py
@@ -4,6 +4,37 @@ from core.sparql.query_form import SelectClause
 
 
 class OBECompact2VerboseConverter:
+    def _expand_for_property(self):
+        """
+        ?Property obe:hasAddress ?Address .
+        OPTIONAL {
+            ?Address ict:hasBuilidingName ?BuildingName .
+        }
+        OPTIONAL {
+            ?Address ict:hasStreet ?Street .
+        }
+        OPTIONAL {
+            ?Address ict:hasStreetNumber ?StreetNumber .
+        }
+        OPTIONAL {
+            ?Address obe:hasUnitName ?UnitName .
+        }
+        OPTIONAL {
+            ?Address obe:hasPostalCode/rdfs:label ?PostalCode
+        }
+        """
+        patterns = [
+            TriplePattern.from_triple("?Property", "obe:hasAddress", "?Address"),
+            OptionalClause([TriplePattern.from_triple("?Address", "ict:hasBuildingName", "?Building")]),
+            OptionalClause([TriplePattern.from_triple("?Address", "ict:hasStreet", "?Street")]),
+            OptionalClause([TriplePattern.from_triple("?Address", "ict:hasStreetNumber", "?StreetNumber")]),
+            OptionalClause([TriplePattern.from_triple("?Address", "obe:hasUnitName", "?UnitName")]),
+            OptionalClause([TriplePattern.from_triple("?Address", "obe:hasPostalCode/rdfs:label", "?PostalCode")])
+        ]
+        vars = ["?Address", "?BuildingName", "?Street", "?StreetNumber", "?UnitName", "?PostalCode"]
+
+        return patterns, vars
+
     def _try_convert_property_hasmeasure_triple(self, pattern: GraphPattern):
         try:
             """
@@ -88,10 +119,17 @@ class OBECompact2VerboseConverter:
             return patterns, vars
         except AssertionError:
             return None
+        
+    
 
     def convert(self, sparql_compact: SparqlQuery):
         select_vars_verbose = list(sparql_compact.select_clause.vars)
-        graph_patterns_verbose = []
+
+        if "?Property" in select_vars_verbose and "?Address" not in select_vars_verbose:
+            graph_patterns_verbose, select_vars = self._expand_for_property()
+            select_vars_verbose.extend(select_vars)
+        else:
+            graph_patterns_verbose = []
 
         for pattern in sparql_compact.graph_patterns:
             optional = self._try_convert_property_hasmeasure_triple(pattern)
